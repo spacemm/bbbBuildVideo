8571d38205fcb82e31f2b7e5b85493638daaca1e
Genaev Maxim <gma@linuxwizard.ru>
2014-03-05 15:42:04 +0400

    ref

--- bbbBuildVideo.java
+++ bbbBuildVideo.java
@@ -370,7 +370,7 @@ public class bbbBuildVideo {
         } catch (IOException e) {
             e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
         }*/
-        List<Map<String, List<Long>>> slots = videoSlot(videoStart, videoStop,startTime,stopTime);
+        List<Map<String, List<Long>>> slots = videoSlot.videoSlot(videoStart, videoStop, startTime, stopTime);
         if(slots.size()>6){
 
             System.out.println("Too much parallel videos");
@@ -440,83 +440,5 @@ public class bbbBuildVideo {
         } catch (IOException e) {
             e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
         }
-    }
-    private static List<Map<String, List<Long>>> videoSlot(Map<String, Long> videoStart, Map<String, Long> videoStop,Long startTime, Long stopTime){
-        List<Map<String, List<Long>>> slots = new ArrayList<>();
-        while(videoStart.size()>0){
-            Map<String,List<Long>> sVideoStart = new LinkedHashMap<String,List<Long>>();
-            sVideoStart=sortByValue(videoStart);
-            //System.out.println(sVideoStart);
-            Map<String,List<Long>> video = new HashMap<String,List<Long>>();
-            for(Map.Entry entry : sVideoStart.entrySet()){
-                List<Long> times = new ArrayList<>();
-                times.add((Long) entry.getValue());
-                times.add(videoStop.get(entry.getKey()));
-                video.put((String) entry.getKey(),times);
-            }
-            System.out.println(video);
-            Map<String,List<Long>> temp = new LinkedHashMap<String,List<Long>>();
-            Long last= Long.valueOf(0);
-            Long lastTime= Long.valueOf(0);
-            Integer c=0;
-            for(Map.Entry entry : sVideoStart.entrySet()){
-                if (c==0){
-                    List<Long> times = new ArrayList<>();
-                    times.add((Long) startTime);
-                    times.add((Long) entry.getValue());
-                    temp.put("start",times);
-                    //last= videoStop.get(entry.getKey());
-                    //lastTime=videoStop.get(entry.getKey());
-                }
-                System.out.println(temp.containsKey(entry.getKey()));
-                if (!temp.containsKey(entry.getKey())){
-                    Long start = (Long) entry.getValue();
-                    System.out.println("last " + last);
-                    System.out.println(start);
-                    if(last<start){
-                        List<Long> times = new ArrayList<>();
-                        times.add(last);
-                        times.add((Long) entry.getValue());
-                        List<Long> times2 = new ArrayList<>();
-                        times2.add((Long) entry.getValue());
-                        times2.add(videoStop.get(entry.getKey()));
-                        last= videoStop.get(entry.getKey());
-                        System.out.println(times);
-                        temp.put((String) entry.getKey(),times2);
-                        temp.put("mid"+entry.getKey(),times);
-                        videoStart.remove(entry.getKey());
-                        lastTime=videoStop.get(entry.getKey());
-                    }
-                }
-                c++;
-            }
-            List<Long> times = new ArrayList<>();
-            times.add(lastTime);
-            times.add(stopTime);
-            temp.put("end" ,times);
-            System.out.println("videostop"+videoStop);
-            System.out.println(videoStart);
-            System.out.println(temp);
-            slots.add(temp);
-        }
-        System.out.println(slots);
-        return slots;
-
-    }
-    static Map sortByValue(Map map) {
-        List list = new LinkedList(map.entrySet());
-        Collections.sort(list, new Comparator() {
-            public int compare(Object o1, Object o2) {
-                return ((Comparable) ((Map.Entry) (o1)).getValue())
-                        .compareTo(((Map.Entry) (o2)).getValue());
-            }
-        });
-
-        Map result = new LinkedHashMap();
-        for (Iterator it = list.iterator(); it.hasNext();) {
-            Map.Entry entry = (Map.Entry)it.next();
-            result.put(entry.getKey(), entry.getValue());
-        }
-        return result;
     }
 }
---
+++ sortByValue.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+/**
+ * Created by space on 3/5/14.
+ */
+public class sortByValue {
+    public static Map sortByValue(Map map) {
+        List list = new LinkedList(map.entrySet());
+        Collections.sort(list, new Comparator() {
+            public int compare(Object o1, Object o2) {
+                return ((Comparable) ((Map.Entry) (o1)).getValue())
+                        .compareTo(((Map.Entry) (o2)).getValue());
+            }
+        });
+
+        Map result = new LinkedHashMap();
+        for (Iterator it = list.iterator(); it.hasNext();) {
+            Map.Entry entry = (Map.Entry)it.next();
+            result.put(entry.getKey(), entry.getValue());
+        }
+        return result;
+    }
+}
